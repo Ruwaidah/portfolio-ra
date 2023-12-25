f83892a8b82730a3ee6f587a09b92c5b44455568
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-12-25 16:20:36 -0600

    adding about me para

--- src/components/AboutMe/About.js
+++ src/components/AboutMe/About.js
@@ -1,6 +1,5 @@
 import "./About.css";
 
-
 const About = () => {
   const back = () => {
     window.scrollBy({
@@ -20,8 +19,23 @@ const About = () => {
       className="About"
       style={{ position: "absolute", top: window.innerHeight }}
     >
-      <h2>About</h2>
       <button onClick={back}>Back</button>
+
+      <h2>About</h2>
+      <p>
+        {" "}
+        I'm a dedicated mom of three wonderful kids, juggling the
+        responsibilities of parenthood with a fulfilling career. I've been
+        working full-time at Walmart as a Personal Shopper since 2022, ensuring
+        that meet the client's preferences and requirements. responsible for
+        selecting fresh produce, checking expiration dates, and making suitable
+        replacements if certain items are out of stock. Beyond my job, I'm also
+        passionate about technology and coding. My coding journey began in 2018,
+        and I've since earned certifications in both Full Stack Development from
+        BloomTech and Web Development from SpringBoard. These achievements
+        reflect my commitment to expanding my skills and staying up-to-date in
+        the ever-evolving world of web development.
+      </p>
       <button onClick={next}>Next</button>
     </div>
   );
