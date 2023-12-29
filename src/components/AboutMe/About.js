dfa6c47b0db828951426241cac753e01e5c4e032
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-12-28 21:19:56 -0600

    fixing next page move

--- src/components/AboutMe/About.js
+++ src/components/AboutMe/About.js
@@ -6,24 +6,21 @@ const About = () => {
       className="About"
       style={{ position: "absolute", top: window.innerHeight }}
     >
-      <div>
-        <h2>About</h2>
-        <p>
-          {" "}
-          I'm a dedicated mom of three wonderful kids, juggling the
-          responsibilities of parenthood with a fulfilling career. I've been
-          working full-time at Walmart as a Personal Shopper since 2022,
-          ensuring that meet the client's preferences and requirements.
-          responsible for selecting fresh produce, checking expiration dates,
-          and making suitable replacements if certain items are out of stock.
-          Beyond my job, I'm also passionate about technology and coding. My
-          coding journey began in 2018, and I've since earned certifications in
-          both Full Stack Development from BloomTech and Web Development from
-          SpringBoard. These achievements reflect my commitment to expanding my
-          skills and staying up-to-date in the ever-evolving world of web
-          development.
-        </p>
-      </div>{" "}
+      <h2>About</h2>
+      <p>
+        {" "}
+        I'm a dedicated mom of three wonderful kids, juggling the
+        responsibilities of parenthood with a fulfilling career. I've been
+        working full-time at Walmart as a Personal Shopper since 2022, ensuring
+        that meet the client's preferences and requirements. responsible for
+        selecting fresh produce, checking expiration dates, and making suitable
+        replacements if certain items are out of stock. Beyond my job, I'm also
+        passionate about technology and coding. My coding journey began in 2018,
+        and I've since earned certifications in both Full Stack Development from
+        BloomTech and Web Development from SpringBoard. These achievements
+        reflect my commitment to expanding my skills and staying up-to-date in
+        the ever-evolving world of web development.
+      </p>
     </div>
   );
 };
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,7 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+  return <div className="NavBar"></div>;
+};
+
+export default NavBar;
