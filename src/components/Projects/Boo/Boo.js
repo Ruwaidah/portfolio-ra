58ec3267594746a8043bffaadb1a25fd7f09d387
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2024-01-02 17:41:56 -0600

    working on the project page

--- src/components/Projects/Boo/Boo.js
+++ src/components/Projects/Boo/Boo.js
@@ -1,18 +1,20 @@
 import boo from "../../../images/Boo.png";
-import "./Boo.css"
+import "./Boo.css";
 
 const Boo = () => {
   return (
-    <div className="Boo-project">
-      <img src={boo} />
+    <div className="project-temp">
       <h4>Boo</h4>
-      <p>
-        Search for and display pet listings based on pet characteristics,
-        location, and status. Search for and display animal welfare
-        organizations based on organization name, ID, and location. display
-        profiles of local organizations. Help users find their perfect pets by
-        location, breed, and other characteristics.
-      </p>
+      <div>
+        <img src={boo} />
+        <p>
+          Search for and display pet listings based on pet characteristics,
+          location, and status. Search for and display animal welfare
+          organizations based on organization name, ID, and location. display
+          profiles of local organizations. Help users find their perfect pets by
+          location, breed, and other characteristics.
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Projects/MovieKnight/MovieKnight.js
+++ src/components/Projects/MovieKnight/MovieKnight.js
@@ -1,16 +1,18 @@
 import "./MovieKnight.css";
-import movieKnight from "../../../images/movieKnight.png"
+import movieKnight from "../../../images/movieKnight.png";
 const MovieKnight = () => {
   return (
-    <div className="MovieKnight-project">
-        <img src={movieKnight} />
+    <div className="project-temp movieknight">
       <h4>Movie Knight</h4>
-      <p>
-        displays movies showing in theaters within the zip code you've search
-        for provides more detailed information about each movie if required
-        allows users to select their preferences for a movie going experience
-        filters available movies that match those preferences for the user
-      </p>
+      <div>
+        <img src={movieKnight} />
+        <p>
+          displays movies showing in theaters within the zip code you've search
+          for provides more detailed information about each movie if required
+          allows users to select their preferences for a movie going experience
+          filters available movies that match those preferences for the user
+        </p>
+      </div>
     </div>
   );
 };
