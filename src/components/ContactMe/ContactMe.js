b17aa497bef2fc09052862eb23c07d6dffaa3576
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2024-01-11 18:00:53 -0600

    adding contact page

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,4 +1,6 @@
 import "./ContactMe.css";
+import github from "../../images/github.png";
+import linkedin from "../../images/linkedin.png";
 
 const ContactMe = () => {
   return (
@@ -6,7 +8,23 @@ const ContactMe = () => {
       className="ContactMe"
       style={{ position: "absolute", top: window.innerHeight * 3 }}
     >
-      <h4>Contact Me</h4>
+      <div className="email-div">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg" />
+        <p> ruwaidah.riyadh@gmail.com</p>
+      </div>{" "}
+      <div className="github-linkedin">
+        <a href="https://github.com/Ruwaidah">
+          <img
+            width="96"
+            height="96"
+            src="https://img.icons8.com/material-outlined/96/FFFFFF/github.png"
+            alt="github"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/ruwaidah-a-930b9a8b/">
+          <img src={linkedin} />
+        </a>
+      </div>{" "}
     </div>
   );
 };
